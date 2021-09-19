bfa17556a0a0c8d349314d777dc79a65ccf35c03
kawsikan <kawsinote@gmail.com>
2021-09-19 11:34:50 +0530

    added first page for wishlist

--- riyasewana/App.js
+++ riyasewana/App.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const App = () => {
+// import HomeScreen from './components/Home';
+// import AddReview from './components/AddReview';
+// import ViewPost from './components/ViewPost';
+import wishlist from './screens/Wishlist';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>
-        Try editing me! 🎉
-      </Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        {/* <Stack.Screen name="Post" component={ViewPost} options={{ title: 'Posts' }} /> */}
+        {/* <Stack.Screen name="Review" component={AddReview} options={{ title: 'Add Review' }} />
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'Reviews' }} /> */}
+        <Stack.Screen name="Wishlist" component={wishlist} options={{ title: 'Wishlist' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-export default App;
